8bbf4c1447722e9b843cd438a8c85d3172f358f9
hhadainn <hhadainn@gmail.com>
2025-07-31 16:07:59 +0900

    minor change

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <aside className="w-64 bg-gradient-to-b from-[#ffd9e0] via-[#f5d5f3] to-[#d9e4ff] p-6 shadow-xl text-black flex flex-col justify-between rounded-tr-3xl rounded-br-3xl">
+    <aside className="w-64 bg-gradient-to-b from-[#ffd9e0] via-[#f5d5f3] to-[#d9e4ff] p-6 shadow-xl text-black flex flex-col justify-between rounded-tr-3xl rounded-br-3xl py-10">
 
       <div>
         <div className="flex items-center gap-3 animate-fadeInOut">
@@ -15,9 +15,9 @@ function Sidebar() {
           </h1>
         </div>
 
-        <div className="text-[14px] text-black text-left leading-snug px-3 py-1 mb-6 space-y-2">
+        <div className="text-[15px] text-black text-left leading-snug px-3 py-10 mb-6 space-y-2">
           <div>
-            <p className="font-semibold text-black mb-1">연애는 기술이 아니라 관계입니다.</p>
+            <p className="font-semibold text-black  mb-1">연애는 기술이 아니라 관계입니다.</p>
             <p className="text-black/80">
               <span className="text-black">그래서 우리는 조언보다 먼저 <br /> 당신의 이야기를</span> 듣는 것부터 시작합니다.
             </p>
@@ -32,11 +32,11 @@ function Sidebar() {
           </div>
         </div>
 
-        <nav className="flex flex-col gap-3">
-          {["메인", "연애 고수의 조언", "데이트 장소", "나의 다이어리", "설정", "로그아웃"].map((label, idx) => (
+        <nav className="flex flex-col gap-3 py-20">
+          {["메인", "연애 고수의 조언", "데이트 장소", "나의 다이어리"].map((label, idx) => (
             <Link
               key={idx}
-              to={['/', '/search', '/dateplace', '/calendar', '/settings', '/logout'][idx]}
+              to={['/', '/search', '/dateplace', '/calendar'][idx]}
               className="rounded-xl px-4 py-2 text-left hover:bg-white/10 transition cursor-pointer flex items-center gap-3 font-medium text-black"
             >
               {label}
